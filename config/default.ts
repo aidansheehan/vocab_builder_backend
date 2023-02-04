a3cf853d89c9100344b11c91a35ba4b35718d1d7
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-02-04 14:58:13 +0000

    VBF-9: comment units on access/refresh token expiry times

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
     port: 8000,
-    accessTokenExpiresIn: 20,   //TOOD how does this tie in with EX set on redis? Can we have SST on this?
-    refreshTokenExpiresIn: 43200,
+    accessTokenExpiresIn: 20,       //minutes until accessToken expiry TODO how does this tie in with EX set on redis? Can we have SST on this?
+    refreshTokenExpiresIn: 43200,   //minutes until refreshToken expiry
     origin: 'http://localhost:8080'
 }
