f36899b31826f1b811263d3e56c7b1fd5fa1a9ea
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-03-14 08:02:15 +0000

    VBB-13 new: implement https reverse proxy in traefik - need to commit to test

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
-    port: 80,
+    port: 8000,
     accessTokenExpiresIn: 20,       //minutes until accessToken expiry TODO how does this tie in with EX set on redis? Can we have SST on this?
     refreshTokenExpiresIn: 43200,   //minutes until refreshToken expiry
     origin: 'http://localhost:8080'
