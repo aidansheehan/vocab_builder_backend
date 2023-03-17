4d8fa6ea763fa877e481e1a1d7524492e2b03e37
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-03-17 08:36:54 +0000

    VBB-7 new: spread users/me response to standardize with other response types, document users/me get request, comment out remaining code related to currently unneeded get all users route

--- src/api/routes/user.route.ts
+++ src/api/routes/user.route.ts
@@ -1,8 +1,8 @@
 import express                              from 'express';
-import { getAllUsersHandler, getMeHandler } from '../controllers/user.controller';
+import { /*getAllUsersHandler,*/ getMeHandler } from '../controllers/user.controller';
 import { deserializeUser }                  from '../middleware/deserializeUser';
 import { requireUser }                      from '../middleware/requireUser';
-import { restrictTo }                       from '../middleware/restrictTo';
+// import { restrictTo }                       from '../middleware/restrictTo';
 
 /**
  * Routes to 
@@ -19,7 +19,56 @@ router.use(deserializeUser, requireUser);
 //Admin get users route
 // router.get('/', restrictTo('admin'), getAllUsersHandler);
 
-// Get my info route
+/**
+ * @openapi
+ * /users/me:
+ *   get:
+ *     summary: Retrieves user details
+ *     tags: ['users']
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: 'Successfully returned user data'
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                       description: The users ID
+ *                       example: 64136604be11d090766433d6
+ *                     email:
+ *                       type: string
+ *                       description: The user's email address.
+ *                       example: 'tom@mail.com'
+ *                     role:
+ *                       type: string
+ *                       description: The user's role.
+ *                       example: user
+ *                     createdAt:
+ *                       type: string
+ *                       description: Date user account created
+ *                       example: 2023-03-16T18:55:00.591Z
+ *                     updatedAt:
+ *                       type: string
+ *                       description: Date user information last updated
+ *                       example: 2023-03-16T18:55:00.591Z
+ *       '401':
+ *         description: User not logged in.
+ *       '500':
+ *         description: Internal server error.
+ *                     
+ */
 router.get('/me', getMeHandler);
 
 export default router;
+
+/**
+ * TODO:
+ *  - add routes for update user information and handlers in user controller
+ */
