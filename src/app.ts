40a79be9fce043fdbb100819cdcccb6b8e75c830
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-03-25 11:08:34 +0000

    VBB-8 new: use api route for docs

--- src/app.ts
+++ src/app.ts
@@ -126,7 +126,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/collections', collectionRouter);
 
 //6. Documentation with Swagger and JSDoc
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //Testing
 app.get('/healthChecker', (req: Request, res: Response, next: NextFunction) => {
