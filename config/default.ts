e518d1b7ba6f7a99b6413c8e3595c17cf1974570
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-05-14 17:58:46 +0100

    VBF-80 new: add dictaglot to cors allowed urls config

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,5 @@ export default {
     port: 8000,
     accessTokenExpiresIn: 20,       //minutes until accessToken expiry TODO how does this tie in with EX set on redis? Can we have SST on this?
     refreshTokenExpiresIn: 43200,   //minutes until refreshToken expiry
-    origin: [ 'https://vocab-builder-framework.vercel.app/', 'http://localhost:8080' ]
+    origin: [ 'https://dictaglot.com', 'https://vocab-builder-framework.vercel.app/', 'http://localhost:8080' ]
 }
